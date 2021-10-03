9b7a8e2a7650f46c87163eb1e72e4ed3fe2317ba
lukka12 <47463180+lukka12@users.noreply.github.com>
2021-10-03 07:47:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ if os.path.exists("local.env"):
 load_dotenv()
 que = {}
 admins = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
+SESSION_NAME = getenv("SESSION_NAME", "BQAhP0SW5LanDKOVWWDlQHU8TMDgJU_sKrADGDw9J75LZKMDWty4vRAGyYSvzL_nb1g6bO9ot8T5FgA0ERaILZfZJGodTwykyeiwFxd4Os1mNxZfImrvoUkGrqEnDGIjNfDJLM2eDhRGQdnNDIltxQOKfEynm0IxG0ORiEdpcAKC4RNH0Vx9uxUwy5D7LemjXAD_TgB40giDO0AQ5KC-7THivVRBTBBRs4AqO1NYRoax1vf8Htr_uGnQdXd0MfmyIVOHqzR2SVRFxOCfwQD9THCUexTIbOymLSkVsIb3GPhwJcmD8HMysKyYYYxK-R7TbWrBvcztQ2qx3Vml8Zrz2_2mds4g1QA")
 ARQ_API_KEY = getenv("ARQ_API_KEY")
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN", "2049904848:AAE6raTlxx9RWgp_22355kTVWJZQzPV9fp8")
 BOT_NAME = getenv("BOT_NAME", "𝐕rⓄs 𝐌𝐮₴𝐢𝐜")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/ca8af7378500099b78075.png")
 AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/64114ef627bfbb87451a9.png")
@@ -25,7 +25,7 @@ ASSISTANT_NAME = getenv("ASSISTANT_NAME", "VrosAssistant")
 GROUP_SUPPORT = getenv("GROUP_SUPPORT", "sanki_bots_support")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "sanki_bots")
 OWNER_NAME = getenv("OWNER_NAME", "sanki_manager") # isi dengan username kamu tanpa simbol @
-DEV_NAME = getenv("DEV_NAME", "sanku_manager")
+DEV_NAME = getenv("DEV_NAME", "sanki_manager")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "180"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
